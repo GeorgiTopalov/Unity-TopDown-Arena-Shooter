12fff99f0abfe501f1cb461f283a454607a29b10
Georgi Topalov <g.s.topalov@gmail.com>
2023-09-01 09:49:59 +0700

    Initial Commit

---
+++ Top Down Arena shooter/Assets/Scripts/TopDownCharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        [SerializeField] public float speed = 10f;
+        public Rigidbody2D rb;
+        public Vector2 movement;
+
+
+        private void Start()
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+        }
+
+
+        private void Update()
+        {
+            movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            moveCharacter(movement);
+        }
+
+        void moveCharacter(Vector2 direction)
+        {
+            rb.velocity = direction * speed * Time.fixedDeltaTime;
+        }
+    }
+}
