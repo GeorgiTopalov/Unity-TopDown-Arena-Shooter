27fbde2c10d4f7cae7aa1654092dbbbd0bb23ae6
Georgi Topalov <g.s.topalov@gmail.com>
2023-09-01 11:59:53 +0700

    Added sprite flip on moving

--- Top Down Arena shooter/Assets/Scripts/TopDownCharacterController.cs
+++ Top Down Arena shooter/Assets/Scripts/TopDownCharacterController.cs
@@ -4,20 +4,23 @@ namespace Cainos.PixelArtTopDown_Basic
 {
     public class TopDownCharacterController : MonoBehaviour
     {
-        [SerializeField] public float speed = 10f;
+        [SerializeField] public float speed = 100f;
         public Rigidbody2D rb;
         public Vector2 movement;
 
+        public SpriteRenderer spriteRenderer;
 
         private void Start()
         {
             rb = this.GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
 
         private void Update()
         {
             movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            FlipSprite();
         }
 
         private void FixedUpdate()
@@ -28,6 +31,20 @@ namespace Cainos.PixelArtTopDown_Basic
         void moveCharacter(Vector2 direction)
         {
             rb.velocity = direction * speed * Time.fixedDeltaTime;
+        }
+
+        void FlipSprite()
+        {
+            float dirX = movement.x;
+
+            if (dirX < 0)
+            {
+                spriteRenderer.flipX = false; 
+            }
+            else if (dirX > 0)
+            {
+                spriteRenderer.flipX = true; 
+            }
         }
     }
 }
